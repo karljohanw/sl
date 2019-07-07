92d04baaa135ac57c0c2b39676be29712e67aae7
Karl Johan Westrin <westrin@kth.se>
2019-07-07 13:12:20 +0200

    Add a fabulous rainbow option implemented by KoffeinFlummi.

--- sl.c
+++ sl.c
@@ -94,19 +94,30 @@ int ONEDIREC  = 1;
 int EVIL      = 0;
 int DISCO     = 0;
 int TGV       = 0;
+int RAINBOW   = 0;
 
 int my_mvaddstr(int y, int x, char *str)
 {
+    int color, err = 0;
     for ( ; x < 0; ++x, ++str)
         if (str == NULL || *str == '\0')  return ERR;
-    for ( ; str != NULL && *str != '\0'; ++str, ++x)
-        if (str!=NULL && mvaddch(y, x, *str) == ERR)  return ERR;
+    for ( ; str != NULL && *str != '\0'; ++str, ++x) {
+        if (RAINBOW == 1) {
+	    color = ((x+y)/5) % 7 + 1;
+            attron(COLOR_PAIR(color));
+        }
+        err = mvaddch(y, x, *str);
+        if (RAINBOW == 1)
+            attroff(COLOR_PAIR(color));
+        if (err == ERR)
+            return ERR;
+    }
     return OK;
 }
 
 int my_mvaddstr_r(int y, int x, char *str)
 {
-  int i = 0;
+  int i = 0, color, err = 0;
   char c;
 
   for ( ; x >= COLS; --x, ++i) {
@@ -136,9 +147,15 @@ case ']':
 c = '[';
   break;
 }
-if (mvaddch(y, x, c) == ERR) {
-  return ERR;
+if (RAINBOW == 1) {
+    color = ((x+y)/5) % 7 + 1;
+    attron(COLOR_PAIR(color));
 }
+err = mvaddch(y, x, c);
+if (RAINBOW == 1)
+    attroff(COLOR_PAIR(color));
+if (err == ERR)
+    return ERR;
 }
 
 return OK;
@@ -163,6 +180,7 @@ void option(char *str)
             case 'e': EVIL     = 1; break;
             case 'D': DISCO    = 1; break;
             case 'G': TGV      = 1; break;
+            case 'r': RAINBOW  = 1; break;
             default:                break;
         }
     }
@@ -180,15 +198,24 @@ int main(int argc, char *argv[])
         }
     }
     initscr();
-    if (DISCO == 1) {
+    if (DISCO == 1 || RAINBOW == 1) {
         start_color();
-        init_pair(7, COLOR_WHITE, COLOR_BLACK);
-        init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
-        init_pair(5, COLOR_BLUE, COLOR_BLACK);
-        init_pair(4, COLOR_RED, COLOR_BLACK);
+	if (can_change_color() || RAINBOW == 1) {
+            init_color(COLOR_RED, 1000, 0, 0);
+            init_color(COLOR_WHITE, 1000, 500, 0);
+            init_color(COLOR_YELLOW, 1000, 1000, 0);
+            init_color(COLOR_GREEN, 0, 1000, 0);
+            init_color(COLOR_CYAN, 300, 300, 1000);
+            init_color(COLOR_BLUE, 0, 0, 1000);
+            init_color(COLOR_MAGENTA, 1000, 0, 1000);
+        }
+        init_pair(1, COLOR_RED, COLOR_BLACK);
+        init_pair(2, COLOR_WHITE, COLOR_BLACK);
         init_pair(3, COLOR_YELLOW, COLOR_BLACK);
-        init_pair(2, COLOR_GREEN, COLOR_BLACK);
-        init_pair(1, COLOR_CYAN, COLOR_BLACK);
+        init_pair(4, COLOR_GREEN, COLOR_BLACK);
+        init_pair(5, COLOR_CYAN, COLOR_BLACK);
+        init_pair(6, COLOR_BLUE, COLOR_BLACK);
+        init_pair(7, COLOR_MAGENTA, COLOR_BLACK);
     }
     if (EVIL==1) {
         signal(SIGINT, SIG_IGN);
@@ -207,7 +234,7 @@ int main(int argc, char *argv[])
       begin_gate(3 * COLS / 10, 0);
     }
     for (x = COLS - 1; ; --x) {
-        if (DISCO && (x + INT_MAX/2) % 7 == 2)
+        if (!RAINBOW && DISCO && (x + INT_MAX/2) % 7 == 2)
             attron(COLOR_PAIR((x + INT_MAX/2) / 16 % 7 + 1));
         if (LOGO == 1) {
             if (add_sl(x, CARS) == ERR) break;
@@ -233,7 +260,7 @@ int main(int argc, char *argv[])
       x_gate(3*COLS/10, 0);
     }
     for (x = 0; ; ++x) {
-        if (DISCO && (x + INT_MAX/2) % 7 == 2)
+        if (!RAINBOW && DISCO && (x + INT_MAX/2) % 7 == 2)
             attron(COLOR_PAIR((x + INT_MAX/2) / 16 % 7 + 1));
         if (LOGO == 1) {
             if (add_sl_r(x, CARS) == ERR) break;
